af5e6f258b67c78428d9fe8e50a8901c3ea16975
Alex Holachek <aholachek@gmail.com>
2020-04-28 11:38:51 -0400

    add commentary

--- src/useTappable.js
+++ src/useTappable.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+/**
+ * A hook to toggle a class on a tappable element that replaces the :active CSS pseudoclass for a nicer UI experience
+ *
+ * @param {Object} options
+ * @param {number} options.delay - Don't show a tap highlight right away to prevent inadvertant triggering (e.g. on scroll)
+ * @param {string} options.tapClass - The name of the class to toggle
+ * @param {boolean} options.preventDefault - since this hook works by delaying the onClick event until the next frame,
+ * we need to specify if we should call event.preventDefault(), since it must be called immediately
+ * @param {onClick} this is the onClick function that we will wrap in order to show the optimum tap highlighting experience
+ */
 export default function useTappable({
   delay = 75,
   tapClass = "tapActive",
-  preventDefault,
+  preventDefault = false,
   onClick,
 } = {}) {
   const timeoutRef = React.useRef();
@@ -30,10 +40,12 @@ export default function useTappable({
     if (!isClick) return;
 
     requestAnimationFrame(() => {
+      // make sure the item will be highlighted if the UI blocks, even if the tap finished within the "delay"
       cachedTarget.classList.add(tapClass);
       requestAnimationFrame(() => {
         onClick(e);
         requestAnimationFrame(() => {
+          // only remove the tapActive class once the UI is no longer blocked
           cachedTarget.classList.remove(tapClass);
         });
       });
